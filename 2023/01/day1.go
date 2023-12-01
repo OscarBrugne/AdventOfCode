369913e0edf0882fd94ed81935567dff3c1b6d70
OscarBrugne <oscar.brugne9@etu.univ-lorraine.fr>
2023-12-01 20:33:00 +0100

    Optimization day 1

--- 2023/01/day1.go
+++ 2023/01/day1.go
@@ -2,71 +2,66 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"unicode"
 
 	"AdventOfCode/utils"
 )
 
-func findDigit(line string, start int, end int, step int) int {
+var spelledDigits = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+func findDigit(line string, start int, end int, step int, isSpelledDigitAllowed bool) int {
 	i := start
 	for (step > 0 && i < end) || (step < 0 && i > end) {
-		var char rune = rune(line[i])
-		if unicode.IsDigit(char) {
+		char := line[i]
+		isDigit := ('0' <= char) && (char <= '9')
+		if isDigit {
 			return int(char - '0')
+		}
+		if isSpelledDigitAllowed {
+			for word, value := range spelledDigits {
+				isSpelledDigit := (i+len(word) <= len(line)) && (line[i:i+len(word)] == word)
+				if isSpelledDigit {
+					return value
+				}
+			}
 		}
 		i += step
 	}
 	return 0
 }
 
-func firstDigit(line string) int {
-	return findDigit(line, 0, len(line), 1)
+func firstDigit(line string, isPart2 bool) int {
+	return findDigit(line, 0, len(line), 1, isPart2)
 }
 
-func lastDigit(line string) int {
-	return findDigit(line, len(line)-1, -1, -1)
+func lastDigit(line string, isPart2 bool) int {
+	return findDigit(line, len(line)-1, -1, -1, isPart2)
 }
 
-func replaceLetterDigits(line string) string {
-	letterDigits := map[string]string{
-		"one":   "o1e",
-		"two":   "t2o",
-		"three": "t3e",
-		"four":  "f4r",
-		"five":  "f5e",
-		"six":   "s6x",
-		"seven": "s7n",
-		"eight": "e8t",
-		"nine":  "n9e",
-	}
-	replacedLine := line
-	for key, element := range letterDigits {
-		replacedLine = strings.Replace(replacedLine, key, element, -1)
-	}
-	return replacedLine
-}
-
-func replaceLetterDigitsInInput(input []string) []string {
-	replacedInput := []string{}
-	for _, line := range input {
-		replacedInput = append(replacedInput, replaceLetterDigits(line))
-	}
-	return replacedInput
-}
-
-func Part1(input []string) int {
+func calculateResult(input []string, isPart2 bool) int {
 	res := 0
 	for _, line := range input {
-		value := 10*firstDigit(line) + lastDigit(line)
+		value := 10*firstDigit(line, isPart2) + lastDigit(line, isPart2)
 		res += value
 	}
 	return res
 }
 
+func Part1(input []string) int {
+	return calculateResult(input, false)
+}
+
 func Part2(input []string) int {
-	newInput := replaceLetterDigitsInInput(input)
-	return Part1(newInput)
+	return calculateResult(input, true)
 }
 
 func main() {
