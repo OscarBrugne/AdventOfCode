a775778ff4e5a21a5db773b69903754312c40ce3
OscarBrugne <oscar.brugne9@etu.univ-lorraine.fr>
2023-12-06 15:54:51 +0100

    Implementation of an int Interval library.

---
+++ utils/intInterval.go
@@ -0,0 +1,138 @@
+package utils
+
+import (
+	"fmt"
+	"sort"
+)
+
+// [Start, End[
+type IntInterval struct{ Start, End int }
+
+type IntIntervalOrIntIntervals interface{}
+
+var EmptyIntInterval = IntInterval{0, 0}
+
+func (ab IntInterval) String() string {
+	return fmt.Sprintf("[%d, %d[", ab.Start, ab.End)
+}
+
+func (ab IntInterval) Equal(cd IntInterval) bool {
+	a, b := ab.Start, ab.End
+	c, d := cd.Start, cd.End
+	return c == a && b == d
+}
+
+func (ab IntInterval) IsIn(intervals []IntInterval) (ok bool, index int) {
+	for index, interval := range intervals {
+		if ab.Equal(interval) {
+			return true, index
+		}
+	}
+	return false, -1
+}
+
+func (ab IntInterval) Contain(cd IntInterval) bool {
+	a, b := ab.Start, ab.End
+	c, d := cd.Start, cd.End
+	return a <= c && d <= b
+}
+
+func (ab IntInterval) Intersection(cd IntInterval) IntInterval {
+	a, b := ab.Start, ab.End
+	c, d := cd.Start, cd.End
+	intersectionStart := max(a, c)
+	intersectionEnd := min(b, d)
+	isOverlapping := intersectionStart < intersectionEnd
+	if isOverlapping {
+		return IntInterval{intersectionStart, intersectionEnd}
+	}
+	return EmptyIntInterval
+}
+
+func (ab IntInterval) Union(cd IntInterval) []IntInterval {
+	a, b := ab.Start, ab.End
+	c, d := cd.Start, cd.End
+	isDisjoint := b < c || d < a
+	if isDisjoint {
+		if a < c {
+			return []IntInterval{ab, cd}
+		}
+		return []IntInterval{cd, ab}
+	}
+	unionStart := min(a, c)
+	unionEnd := max(b, d)
+	return []IntInterval{{unionStart, unionEnd}}
+}
+
+func (ab IntInterval) Difference(cd IntInterval) []IntInterval {
+	a, b := ab.Start, ab.End
+	c, d := cd.Start, cd.End
+	if cd.Contain(ab) {
+		return []IntInterval{}
+	}
+	var difference []IntInterval
+	if a < c {
+		End := min(b, c)
+		difference = append(difference, IntInterval{a, End})
+	}
+	if b > d {
+		Start := max(a, d)
+		difference = append(difference, IntInterval{Start, b})
+	}
+	return difference
+}
+
+func (ab IntInterval) DisjointUnion(cd IntInterval) []IntInterval {
+	a, b := ab.Start, ab.End
+	c, d := cd.Start, cd.End
+	isDisjoint := b < c || d < a
+	if isDisjoint {
+		if a < c {
+			return []IntInterval{ab, cd}
+		}
+		return []IntInterval{cd, ab}
+	}
+	var disjointUnion []IntInterval
+	if a < c {
+		disjointUnion = append(disjointUnion, IntInterval{a, c})
+	} else {
+		disjointUnion = append(disjointUnion, IntInterval{c, a})
+	}
+	if b < d {
+		disjointUnion = append(disjointUnion, IntInterval{b, d})
+	} else {
+		disjointUnion = append(disjointUnion, IntInterval{d, b})
+	}
+	return disjointUnion
+}
+
+func SortIntIntervals(intervals []IntInterval) {
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i].Start != intervals[j].Start {
+			return intervals[i].Start < intervals[j].Start
+		}
+		return intervals[i].End < intervals[j].End
+	})
+}
+
+func MergeIntIntervals(intervals []IntInterval) []IntInterval {
+	if len(intervals) <= 0 {
+		return nil
+	}
+	SortIntIntervals(intervals)
+	merged := []IntInterval{}
+	merged = append(merged, intervals[0])
+	for i := 1; i < len(intervals); i++ {
+		current := intervals[i]
+		lastMerged := merged[len(merged)-1]
+		if current.Start <= lastMerged.End {
+			if current.End > lastMerged.End {
+				lastMerged.End = current.End
+				merged[len(merged)-1] = lastMerged
+			}
+		} else {
+			merged = append(merged, current)
+		}
+	}
+	return merged
+}
---
+++ utils/intInterval_test.go
@@ -0,0 +1,175 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIntInterval_String(t *testing.T) {
+	interval := IntInterval{Start: 2, End: 5}
+	expected := "[2, 5["
+	if result := interval.String(); result != expected {
+		t.Errorf("String representation is incorrect, got: %s, expected: %s.", result, expected)
+	}
+}
+
+func TestIntInterval_Equal(t *testing.T) {
+	interval1 := IntInterval{Start: 2, End: 5}
+	interval2 := IntInterval{Start: 2, End: 5}
+	interval3 := IntInterval{Start: 3, End: 6}
+	if !interval1.Equal(interval2) {
+		t.Errorf("Expected intervals %s and %s to be equal, but they were not.", interval1, interval2)
+	}
+	if interval1.Equal(interval3) {
+		t.Errorf("Expected intervals %s and %s to be unequal, but they were equal.", interval1, interval2)
+	}
+}
+
+func TestSortIntIntervals(t *testing.T) {
+	intervals := []IntInterval{
+		{Start: 5, End: 1},
+		{Start: 1, End: 4},
+		{Start: 2, End: 8},
+		{Start: 2, End: 6},
+	}
+	expected := []IntInterval{
+		{Start: 1, End: 4},
+		{Start: 2, End: 6},
+		{Start: 2, End: 8},
+		{Start: 5, End: 1},
+	}
+	SortIntIntervals(intervals)
+	if !reflect.DeepEqual(intervals, expected) {
+		t.Errorf("Intervals were not sorted as expected, got: %v, want: %v.", intervals, expected)
+	}
+}
+
+func TestIsIn(t *testing.T) {
+	intervals := []IntInterval{
+		{Start: 1, End: 5},
+		{Start: 6, End: 10},
+		{Start: 11, End: 15},
+	}
+	interval := IntInterval{Start: 6, End: 10}
+	exists, index := interval.IsIn(intervals)
+	if !exists || index != 1 {
+		t.Errorf("Interval %v not found in the slice %v.", interval, intervals)
+	}
+	interval = IntInterval{Start: 16, End: 20}
+	exists, index = interval.IsIn(intervals)
+	if exists || index != -1 {
+		t.Errorf("Interval %v found in the slice %v, but it shouldn't exist.", interval, intervals)
+	}
+}
+
+func TestContain(t *testing.T) {
+	interval1 := IntInterval{Start: 2, End: 6}
+	interval2 := IntInterval{Start: 3, End: 5}
+	interval3 := IntInterval{Start: 7, End: 9}
+	if !interval1.Contain(interval2) {
+		t.Errorf("Expected %v to contain %v, but it did not.", interval1, interval2)
+	}
+	if interval1.Contain(interval3) {
+		t.Errorf("Expected %v to not contain %v, but it did.", interval1, interval3)
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	interval1 := IntInterval{Start: 2, End: 5}
+	interval2 := IntInterval{Start: 4, End: 7}
+	expected := IntInterval{Start: 4, End: 5}
+	result := interval1.Intersection(interval2)
+	if !result.Equal(expected) {
+		t.Errorf("Intersection was incorrect, got: %v, expected: %v.", result, expected)
+	}
+	interval3 := IntInterval{Start: 6, End: 8}
+	result = interval1.Intersection(interval3)
+	if !result.Equal(EmptyIntInterval) {
+		t.Errorf("Expected empty interval for non-intersecting intervals, got: %v", result)
+	}
+}
+
+func TestUnion(t *testing.T) {
+	interval1 := IntInterval{Start: 2, End: 5}
+	interval2 := IntInterval{Start: 4, End: 7}
+	expected := []IntInterval{{Start: 2, End: 7}}
+	result := interval1.Union(interval2)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Union was incorrect, got: %v, expected: %v.", result, expected)
+	}
+	interval3 := IntInterval{Start: 8, End: 10}
+	result = interval1.Union(interval3)
+	if len(result) != 2 {
+		t.Errorf("Expected disjoint intervals for non-overlapping intervals, got: %v.", result)
+	}
+}
+
+func TestDifference(t *testing.T) {
+	interval1 := IntInterval{Start: 2, End: 7}
+	interval2 := IntInterval{Start: 4, End: 6}
+	expected := []IntInterval{{Start: 2, End: 4}, {Start: 6, End: 7}}
+	result := interval1.Difference(interval2)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("Difference was incorrect, got: %v, expected: %v.", result, expected)
+	}
+	interval3 := IntInterval{Start: 1, End: 9}
+	result = interval1.Difference(interval3)
+	if len(result) != 0 {
+		t.Errorf("Expected no difference for fully contained interval, got: %v.", result)
+	}
+}
+
+func TestDisjointUnion(t *testing.T) {
+	interval1 := IntInterval{Start: 2, End: 5}
+	interval2 := IntInterval{Start: 7, End: 9}
+	expected := []IntInterval{{Start: 2, End: 5}, {Start: 7, End: 9}}
+	result := interval1.DisjointUnion(interval2)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("DisjointUnion was incorrect, got: %v, expected: %v.", result, expected)
+	}
+	interval3 := IntInterval{Start: 4, End: 8}
+	result = interval1.DisjointUnion(interval3)
+	if len(result) != 2 {
+		t.Errorf("Expected disjoint intervals for intersecting intervals, got: %v.", result)
+	}
+}
+
+func TestMergeIntIntervals(t *testing.T) {
+	intervals1 := []IntInterval{
+		{Start: 1, End: 3},
+		{Start: 2, End: 4},
+		{Start: 5, End: 7},
+	}
+	expected1 := []IntInterval{
+		{Start: 1, End: 4},
+		{Start: 5, End: 7},
+	}
+	result1 := MergeIntIntervals(intervals1)
+	if !reflect.DeepEqual(result1, expected1) {
+		t.Errorf("MergeIntIntervals failed, expected: %v, got: %v", expected1, result1)
+	}
+	intervals2 := []IntInterval{
+		{Start: 2, End: 4},
+		{Start: 6, End: 8},
+		{Start: 10, End: 12},
+	}
+	expected2 := []IntInterval{
+		{Start: 2, End: 4},
+		{Start: 6, End: 8},
+		{Start: 10, End: 12},
+	}
+	result2 := MergeIntIntervals(intervals2)
+	if !reflect.DeepEqual(result2, expected2) {
+		t.Errorf("MergeIntIntervals failed, expected: %v, got: %v", expected2, result2)
+	}
+	intervals3 := []IntInterval{
+		{Start: 1, End: 3},
+		{Start: 2, End: 4},
+		{Start: 4, End: 7},
+	}
+	expected3 := IntInterval{Start: 1, End: 7}
+	result3 := MergeIntIntervals(intervals3)[0]
+	if !reflect.DeepEqual(result3, expected3) {
+		t.Errorf("MergeIntIntervals failed, expected: %v, got: %v", expected3, result3)
+	}
+}
