09d50067a008c47d7df1fdfa05e56eb9790ce6be
OscarBrugne <oscar.brugne9@etu.univ-lorraine.fr>
2023-12-17 17:01:49 +0100

    Day 17 **

--- 2023/17/day17.go
+++ 2023/17/day17.go
@@ -17,6 +17,14 @@ func (c1 Coord) Add(c2 Coord) Coord {
 	return Coord{c1.X + c2.X, c1.Y + c2.Y}
 }
 
+func (c1 Coord) Substract(c2 Coord) Coord {
+	return Coord{c1.X - c2.X, c1.Y - c2.Y}
+}
+
+func (c1 Coord) opposite() Coord {
+	return Coord{-c1.X, -c1.Y}
+}
+
 type Grid struct {
 	Width  int
 	Height int
@@ -111,25 +119,122 @@ func (grid Grid) dijkstra(start Coord, goal Coord) (map[Coord]Coord, map[Coord]i
 	return cameFrom, costSoFar
 }
 
+func (grid Grid) dijkstraConstrained(start Coord, goal Coord, minStraight int, maxStraight int) int {
+	type Info struct {
+		coord       Coord
+		dir         Coord
+		numStraight int
+	}
+	startInfo := Info{coord: start, dir: Coord{}, numStraight: 0}
+
+	frontier := &utils.CostQueue{}
+	heap.Init(frontier)
+	heap.Push(frontier, utils.CostQueueItem{Item: startInfo, Cost: 0})
+
+	costSoFar := make(map[Info]int)
+	costSoFar[startInfo] = 0
+
+	for frontier.Len() > 0 {
+		minItem := heap.Pop(frontier).(utils.CostQueueItem)
+		current := minItem.Item.(Info)
+		currentCost := minItem.Cost
+
+		if current.coord == goal {
+			return currentCost
+		}
+
+		for _, next := range grid.neighbors4(current.coord) {
+			newDir := next.Substract(current.coord)
+			newNumStraight := 1
+			if newDir == current.dir {
+				newNumStraight += current.numStraight
+			}
+
+			nextInfo := Info{
+				coord:       next,
+				dir:         newDir,
+				numStraight: newNumStraight,
+			}
+			newCost := currentCost + grid.Data[next]
+			actualCost, isFound := costSoFar[nextInfo]
+
+			isValid := (!isFound || newCost < actualCost) &&
+				(current.numStraight >= minStraight || newDir == current.dir || current.coord == start) &&
+				(newNumStraight <= maxStraight) &&
+				(newDir != current.dir.opposite())
+			if isValid {
+				costSoFar[nextInfo] = newCost
+
+				queueItem := utils.CostQueueItem{Item: nextInfo, Cost: newCost}
+				heap.Push(frontier, queueItem)
+			}
+		}
+	}
+
+	return -1
+}
+
+func reconstructPath(cameFrom map[Coord]Coord, start Coord, goal Coord) []Coord {
+	path := []Coord{goal}
+
+	current := goal
+	for current != start {
+		fmt.Println(current)
+		current = cameFrom[current]
+		path = append([]Coord{current}, path...)
+	}
+
+	return path
+}
+
+func buildPathGrid(grid Grid, path []Coord) Grid {
+	pathGrid := Grid{
+		Width:  grid.Width,
+		Height: grid.Height,
+		Data:   grid.Data,
+	}
+
+	for i := 0; i < len(path)-1; i++ {
+		prevCoord := path[i]
+		currCoord := path[i+1]
+		dir := currCoord.Substract(prevCoord)
+		switch dir {
+		case North:
+			pathGrid.Data[currCoord] = '^' - '0'
+		case East:
+			pathGrid.Data[currCoord] = '>' - '0'
+		case South:
+			pathGrid.Data[currCoord] = 'v' - '0'
+		case West:
+			pathGrid.Data[currCoord] = '<' - '0'
+		}
+	}
+
+	return pathGrid
+}
+
 func Part1(input []string) int {
 	grid := buildGrid(input)
-	fmt.Println(grid.toString())
 
 	start := Coord{0, 0}
 	goal := Coord{grid.Width - 1, grid.Height - 1}
-	_, costSoFar := grid.dijkstra(start, goal)
-	res := costSoFar[goal]
+	res := grid.dijkstraConstrained(start, goal, 0, 3)
 
 	return res
 }
 
 func Part2(input []string) int {
-	res := 0
+	grid := buildGrid(input)
+
+	start := Coord{0, 0}
+	goal := Coord{grid.Width - 1, grid.Height - 1}
+	res := grid.dijkstraConstrained(start, goal, 4, 10)
+
 	return res
 }
 
 func main() {
-	fileName := "input_test.txt"
+	fileName := "input.txt"
 	input := utils.ReadFile(fileName)
 
 	start1 := time.Now()
